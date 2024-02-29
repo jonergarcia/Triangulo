53147a6b703612eb9495f091f9313238fb336a11
Jon Garcia <jonergarcia@hotmail.com>
2024-02-29 18:45:34 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CalcularArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo();
+
+            Console.WriteLine("Digite o valor de Largura: ");
+            triangulo.Largura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o Valor da Altura: ");
+            triangulo.Altura = double.Parse(Console.ReadLine());
+
+            double area = triangulo.CalcularArea();
+            Console.WriteLine($"A área é: {area}.");
+
+            double perimetro = triangulo.CalcularPerimetro();
+            Console.WriteLine($"O perímetro é {perimetro}.");
+
+            double diagonal = triangulo.CalcularDiagonal();
+            Console.WriteLine($"A Diagonal é: {diagonal}");
+        }
+    }
+}
+
---
+++ Triangulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CalcularArea
+{
+    class Triangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double CalcularArea()
+        {
+            double area = Altura * Largura;
+            return area;
+        }
+
+        public double CalcularPerimetro()
+        {
+            double perimetro = 2 * (Largura + Altura);
+            return perimetro;
+        }
+
+        public double CalcularDiagonal()
+        {
+            double diagonal = Math.Sqrt(Math.Pow(Altura, 2) + Math.Pow(Largura, 2));
+            return diagonal;
+        }
+    }
+}
